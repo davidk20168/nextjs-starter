ff2c85187e4eccc32c6ecf9e8d1c43253b7f1ad6
davidk20168 <51851119+davidk20168@users.noreply.github.com>
2025-10-07 07:18:02 +0000

    baru

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,6 @@
 import "./globals.css";
 import type { Metadata } from "next";
-import Nav from "@/components/Nav";
+import Nav from "../components/Nav";
 
 export const metadata: Metadata = {
   title: process.env.NEXT_PUBLIC_SITE_NAME || "Next.js Starter",
